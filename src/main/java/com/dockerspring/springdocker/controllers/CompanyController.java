cbb8a6f1daa8b600092ecd261dfdd8edd027d790
LB-Okami <lucabtei@outlook.com>
2023-08-02 00:21:34 -0300

    First method

---
+++ src/main/java/com/dockerspring/springdocker/controllers/CompanyController.java
@@ -0,0 +1,34 @@
+package com.dockerspring.springdocker.controllers;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dockerspring.springdocker.model.Company;
+import com.dockerspring.springdocker.services.CompanyService;
+
+@Controller
+@RequestMapping("/companies")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CompanyController {
+    @Autowired
+    private CompanyService companyService;
+
+    @PostMapping
+    public ResponseEntity<Company> save(@RequestBody Company company) {
+
+        Optional<Company> optionalCompany = companyService.saveCompany(company);
+		try {
+			return ResponseEntity.ok(optionalCompany.get());
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
+        
+    }
+}
--- src/main/java/com/dockerspring/springdocker/services/CompanyService.java
+++ src/main/java/com/dockerspring/springdocker/services/CompanyService.java
@@ -1,8 +1,11 @@
 package com.dockerspring.springdocker.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dockerspring.springdocker.model.Company;
 import com.dockerspring.springdocker.repositories.CompanyRepository;
 
 @Service
@@ -10,5 +13,7 @@ public class CompanyService {
     @Autowired
     private CompanyRepository companyRepository;
 
-    
+    public Optional<Company> saveCompany(Company company) {
+        return Optional.of(companyRepository.save(company));
+    }
 }
